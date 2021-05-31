ece51b58cfa44c325ebff534f54ee5b09967d440
AishwaryaLondhe12 <79899671+AishwaryaLondhe12@users.noreply.github.com>
2021-06-01 00:06:13 +0530

    Update repoApi.js

--- app/services/repoApi.js
+++ app/services/repoApi.js
@@ -1,4 +1,4 @@
 import { generateApiClient } from '@utils/apiUtils';
-const repoApi = generateApiClient('github');
+const repoApi = generateApiClient('itunes');
 
-export const getRepos = repoName => repoApi.get(`/search/repositories?q=${repoName}`);
+export const getRepos = artist => repoApi.get(`/search/?term=${artist}&entity=musicVideo`);
